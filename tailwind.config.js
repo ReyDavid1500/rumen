59c9110f77c9f755ad20dda94b9d6764c7f0973c
David Guzman <davidguzman1500@gmail.com>
2023-11-30 14:32:06 -0300

    firs page design

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "buttons": 'Permanent Marker, cursive',
+        "body": 'Saira Extra Condensed, sans-serif'
+      },
+    },
+  },
+  plugins: [],
+}
+
