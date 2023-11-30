34fd456c4cc7224a058803a75eac8a6f55925e23
David Guzman <davidguzman1500@gmail.com>
2023-11-30 15:46:07 -0300

    file structure and layot

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,60 @@
+export function Footer() {
+  return (
+    <footer className="bg-gray-800 pt-10 pl-3">
+      <div className="text-white  flex flex-col gap-3 pb-10 sm:flex-row-reverse sm:justify-center sm:gap-20">
+        <div className="sign-up">
+          <form className="flex flex-row gap-4 text-white font-medium">
+            <input
+              type="text"
+              placeholder="Ingresa tu email"
+              className="bg-inherit border-b-2"
+            />
+            <div className="bg-orange-500/80 hover:bg-orange-500 text-white font-bold p-2 rounded-md font-buttons">
+              <a target="_blank" rel="noopener noreferrer" href="/">
+                Registrate!!
+              </a>
+            </div>
+          </form>
+        </div>
+        <section>
+          <div className="social-media mb-3 text-green-800 font-bold">
+            <ul className="flex flex-row gap-3">
+              <li className="hover:text-yellow-800/60 hover:font-extrabold transition duration-1000 delay-500">
+                <a href="/">
+                  <p>FACEBOOK</p>
+                </a>
+              </li>
+              <li className="hover:text-yellow-800/60 hover:font-extrabold transition duration-1000 delay-500">
+                <a href="/">
+                  <p>INSTAGRAM</p>
+                </a>
+              </li>
+              <li className="hover:text-yellow-800/60 hover:font-extrabold transition-all duration-1000 delay-500">
+                <a href="/">
+                  <p>CONTACTO</p>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="contact-info flex flex-col gap-3 text-[#A7A69C]">
+            <div className="flex flex-row gap-3">
+              <p>Pucon, La Araucanía</p>
+              <p>+56 9 84541821</p>
+            </div>
+            <div>
+              <p>
+                LUNES <span>CERRADOS</span>
+              </p>
+              <p>
+                MARTES A DOMINGO <span>12 A 18 HRS</span>
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div className="copy-right text-white text-sm">
+        <p className="pb-3">© 2023 Rumen Carnes Ahumadas</p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,26 @@
+import { Layout } from "../../Components/Layout";
+
+function Home() {
+  return (
+    <Layout>
+      <main>
+        <div className="overflow-hidden max-h-[500px]">
+          <img
+            src="/Rumen-15.jpg"
+            alt="Rumen Principal"
+            className="w-full h-auto"
+          />
+        </div>
+        <section className="content-section">
+          <div className="text-[10vw] sm:text-[6vw] md:text-[4vw] p-3 font-medium mt-3 mb-3">
+            <h1 className="text-center">
+              "LA RECETA DE LA FELICIDAD <br /> LLEVA 12 HORAS DE AHUMADO."
+            </h1>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
