f9627ff33a88c8f07f7a986f56d325979b437225
David Guzman <davidguzman1500@gmail.com>
2023-11-30 21:14:16 -0300

    menu component updated

--- src/Components/Menu/index.tsx
+++ src/Components/Menu/index.tsx
@@ -1,7 +1,10 @@
 export function Menu() {
   return (
-    <div>
-      <h1>Menu</h1>
+    <div className="pt-2 md:pt-10 xl:pt-16 pb-16 bg-[url('/wood.jpg')]">
+      <div className="w-[90vw] md:w-[70vw] xl:w-[60vw] m-auto">
+        <img src="/menu1.jpg" alt="Menu" />
+        <img src="/menu2.jpg" alt="Menu" />
+      </div>
     </div>
   );
 }
