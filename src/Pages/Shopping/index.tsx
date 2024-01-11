0d15dd73df6d1f9210b893991e5f803fe2652891
David Guzman <davidguzman1500@gmail.com>
2024-01-10 22:39:35 -0300

    shopping products render by category

--- src/Pages/Shopping/index.tsx
+++ src/Pages/Shopping/index.tsx
@@ -10,13 +10,16 @@ function Shopping() {
 
   useEffect(() => {
     setIsLoading(true);
-    fetch("https://rumen-server.onrender.com/products")
+    fetch(`https://rumen-server.onrender.com/products`)
       .then((res) => res.json())
       .then((data) => {
         setProducts(data);
         setIsLoading(false);
       });
   }, []);
+
+  const categories = products.map((product) => product.category);
+  const categoryArray = [...new Set(categories)];
 
   return (
     <ShopLayout>
@@ -25,21 +28,32 @@ function Shopping() {
       ) : (
         <div className="md:flex md:flex-row md:justify-between p-4">
           <div className="p-4 bg-white w-[90vw] m-auto mt-4 md:w-[70vw] md:m-0">
-            <h1 className="text-center font-bold text-2xl mb-[50px]">
-              Carnes Ahumadas y Sandwich
-            </h1>
-            <div className="card mt-4">
-              {products.map((product) => (
-                <Card
-                  key={product._id}
-                  _id={product._id}
-                  image={product.image}
-                  price={product.price}
-                  name={product.name}
-                  description={product.description}
-                />
-              ))}
-            </div>
+            {categoryArray.map((category, index) => (
+              <>
+                <h1
+                  key={index}
+                  className="text-center font-bold text-4xl mb-[50px] mt-8"
+                >
+                  {category}
+                </h1>
+                <div className="card mt-4">
+                  {products.map((product) => {
+                    if (product.category === category) {
+                      return (
+                        <Card
+                          key={product._id}
+                          _id={product._id}
+                          image={product.image}
+                          price={product.price}
+                          name={product.name}
+                          description={product.description}
+                        />
+                      );
+                    }
+                  })}
+                </div>
+              </>
+            ))}
           </div>
           <OrderSummary
             title="PEDIDO"
