77c09667bf020c175c7d1c5168e568e88d7845d7
David Guzman <davidguzman1500@gmail.com>
2024-02-01 14:36:55 -0300

    signup yup validation implemented

--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -1,7 +1,47 @@
 import { Link } from "react-router-dom";
 import ShopLayout from "../../Components/ShopLayout";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type SignUpData = {
+  name: string;
+  email: string;
+  address: string;
+  password: string;
+  validatePassword: string;
+};
+
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .required("El nombre es requerido")
+    .min(3, "El nombre debe tener mas de 2 letras"),
+  email: yup
+    .string()
+    .email("No es un email valido")
+    .required("El email es requerido"),
+  address: yup.string().required("La dirección es requerido").max(100),
+  password: yup.string().required("La contraseña es requerido").min(8).max(20),
+  validatePassword: yup
+    .string()
+    .required("Validar la contraseña es requerido")
+    .min(8)
+    .max(20)
+    .oneOf([yup.ref("password")], "Password no coincide"),
+});
 
 function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpData, any>({ resolver: yupResolver(schema) });
+
+  const handlerSubmit = (data: SignUpData) => {
+    console.log(data);
+  };
+
   return (
     <ShopLayout>
       <>
@@ -12,51 +52,81 @@ function SignUp() {
             </h1>
           </header>
           <main className="p-6">
-            <form className="flex flex-col gap-4">
-              <input
-                className="border-2 border-gray-200 p-2 rounded-lg w-[100%]"
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Nombre Completo"
-              />
-              <input
-                className="border-2 border-gray-200 p-2 rounded-lg w-[100%]"
-                type="text"
-                name="email"
-                id="email"
-                placeholder="Email"
-              />
+            <form
+              onSubmit={handleSubmit(handlerSubmit)}
+              className="sign-up flex flex-col gap-4"
+            >
+              <div className="flex flex-col justify-between gap-4 sm:flex-row">
+                <div className="sm:w-[50%]">
+                  <input
+                    {...register("name")}
+                    className="border-2 border-gray-200 p-2 rounded-lg w-[100%]"
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Nombre Completo"
+                  />
+                  <p className="text-sm text-red-500 font-medium m-auto">
+                    {errors.name?.message}
+                  </p>
+                </div>
+                <div className="sm:w-[50%]">
+                  <input
+                    {...register("email")}
+                    className="border-2 border-gray-200 p-2 rounded-lg w-[100%]"
+                    type="text"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                  />
+                  <p className="text-sm text-red-500 font-medium m-auto">
+                    {errors.email?.message}
+                  </p>
+                </div>
+              </div>
+
               <textarea
+                {...register("address")}
                 className="border-2 border-gray-200 p-2 rounded-lg w-[100%]"
                 name="address"
                 id="address"
                 placeholder="Dirección de despacho en Pucón!"
               />
+              <p className="text-sm text-red-500 font-medium m-auto">
+                {errors.address?.message}
+              </p>
               <input
+                {...register("password")}
                 className="border-2 border-gray-200 p-2 rounded-lg w-[100%]"
                 type="password"
                 name="password"
                 id="password"
                 placeholder="Contraseña"
               />
+              <p className="text-sm text-red-500 font-medium m-auto">
+                {errors.password?.message}
+              </p>
               <input
+                {...register("validatePassword")}
                 className="border-2 border-gray-200 p-2 rounded-lg w-[100%]"
                 type="password"
-                name="password"
-                id="password"
+                name="validatePassword"
+                id="validatePassword"
                 placeholder="Confirma tu contraseña"
               />
-              <Link
-                to="/order"
-                className="bg-regular-blue text-white p-2 font-bold rounded-lg hover:bg-regular-blue/50 w-[80%] m-auto text-center"
-              >
+              <p className="text-sm text-red-500 font-medium m-auto">
+                {errors.validatePassword?.message}
+              </p>
+              <button className="bg-regular-blue text-white p-2 font-bold rounded-lg hover:bg-regular-blue/50 w-[80%] m-auto text-center">
                 Registrate!
-              </Link>
+              </button>
               <div className="flex flex-row gap-1 items-center">
                 <p className="text-xs">Ya estas registrado?</p>
-                <Link className="text-xs font-bold text-rumen-orange" to="">
-                  Ingresa tus datos aquí!
+                <Link
+                  className="text-xs font-bold text-rumen-orange"
+                  to="/shopping"
+                >
+                  Vuelve a los productos!
                 </Link>
               </div>
             </form>
