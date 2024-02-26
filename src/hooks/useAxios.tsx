86cb0cbc5cf57aff2a98aaeb075e06b97d44927c
David Guzman <davidguzman1500@gmail.com>
2024-02-25 21:57:07 -0300

    baseURL did not work, will implement dotenv

--- src/hooks/useAxios.tsx
+++ src/hooks/useAxios.tsx
@@ -11,7 +11,7 @@ export const useAxios = () => {
   }, []);
 
   const requester = axios.create({
-    baseURL: "http://localhost:3000" || "https://rumen-server.onrender.com",
+    baseURL: "https://rumen-server.onrender.com",
     headers: {
       "content-type": "application/json",
       Authorization: `Bearer ${savedToken?.access_token}`,
